956bb579ccfe99d76bbff64ec66e993be4849c5f
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2022-05-26 15:53:54 +0100

    Add simple mutation
    A proof-of-concept mutation that replaces + with -.

--- src/libdredd/include_private/include/libdredd/mutate_visitor.h
+++ src/libdredd/include_private/include/libdredd/mutate_visitor.h
@@ -15,9 +15,12 @@
 #ifndef DREDD_MUTATE_VISITOR_H
 #define DREDD_MUTATE_VISITOR_H
 
+#include <cassert>
+#include <utility>
 #include <vector>
 
 #include "clang/AST/ASTContext.h"
+#include "clang/AST/Decl.h"
 #include "clang/AST/DeclBase.h"
 #include "clang/AST/DeclCXX.h"
 #include "clang/AST/DeclObjC.h"
@@ -25,11 +28,14 @@
 #include "clang/AST/DeclTemplate.h"
 #include "clang/AST/Expr.h"
 #include "clang/AST/OpenMPClause.h"
+#include "clang/AST/OperationKinds.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/Redeclarable.h"
 #include "clang/AST/Stmt.h"
 #include "clang/AST/StmtIterator.h"
 #include "clang/AST/Type.h"
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "llvm/ADT/ArrayRef.h"
 
@@ -39,13 +45,99 @@ class MutateVisitor : public clang::RecursiveASTVisitor<MutateVisitor> {
  public:
   explicit MutateVisitor(const clang::CompilerInstance& compiler_instance);
 
-  bool TraverseBinaryOperator(clang::BinaryOperator* binary_operator) {
-    RecursiveASTVisitor<MutateVisitor>::TraverseBinaryOperator(binary_operator);
+  bool TraverseFunctionDecl(clang::FunctionDecl* function_decl) {
+    // Check whether this function declaration is in the main source file. If it
+    // is not, do not traverse it. If it is, record that it is the enclosing
+    // function while traversing it, so that declarations arising from mutations
+    // inside the function can be inserted directly before it.
+
+    const clang::SourceManager& source_manager =
+        ast_context_.getSourceManager();
+    auto begin_file_id =
+        source_manager.getFileID(function_decl->getSourceRange().getBegin());
+    auto end_file_id =
+        source_manager.getFileID(function_decl->getSourceRange().getEnd());
+    auto main_file_id = source_manager.getMainFileID();
+    if (begin_file_id != main_file_id || end_file_id != main_file_id) {
+      // This indicates either that the function is not in the main file, or
+      // that it is inside a macro. Either way, skip it.
+      return true;
+    }
+    // Traverse the function, recording that it is the enclosing function during
+    // traversal.
+    assert(enclosing_function_ == nullptr);
+    enclosing_function_ = function_decl;
+    RecursiveASTVisitor::TraverseFunctionDecl(function_decl);
+    assert(enclosing_function_ == function_decl);
+    enclosing_function_ = nullptr;
     return true;
+  }
+
+  bool TraverseBinaryOperator(clang::BinaryOperator* binary_operator) {
+    // As a proof of concept, this records opportunities to turn a + node into a
+    // - node.
+
+    // In order to ensure that mutation opportunities are presented bottom-up,
+    // traverse the AST node before processing it.
+    RecursiveASTVisitor<MutateVisitor>::TraverseBinaryOperator(binary_operator);
+
+    // First, check that the binary expression and its arguments have source
+    // ranges that are part of the main file. In particular, this avoids
+    // mutating expressions that directly involve the use of macros (though it
+    // is OK if sub-expressions of arguments use macros).
+    const clang::SourceManager& source_manager =
+        ast_context_.getSourceManager();
+    auto begin_file_id =
+        source_manager.getFileID(binary_operator->getSourceRange().getBegin());
+    auto end_file_id =
+        source_manager.getFileID(binary_operator->getSourceRange().getEnd());
+
+    auto lhs_begin_file_id = source_manager.getFileID(
+        binary_operator->getLHS()->getSourceRange().getBegin());
+    auto lhs_end_file_id = source_manager.getFileID(
+        binary_operator->getLHS()->getSourceRange().getEnd());
+
+    auto rhs_begin_file_id = source_manager.getFileID(
+        binary_operator->getRHS()->getSourceRange().getBegin());
+    auto rhs_end_file_id = source_manager.getFileID(
+        binary_operator->getRHS()->getSourceRange().getEnd());
+
+    auto main_file_id = source_manager.getMainFileID();
+
+    // TODO(afd): It is likely that a lot of checks such as this will be
+    // required throughout the code-base, so a helper function would make sense.
+    if (!(main_file_id == begin_file_id && main_file_id == end_file_id &&
+          main_file_id == lhs_begin_file_id &&
+          main_file_id == lhs_end_file_id &&
+          main_file_id == rhs_begin_file_id &&
+          main_file_id == rhs_end_file_id)) {
+      return true;
+    }
+    // At the moment, restrict attention to +
+    if (binary_operator->getOpcode() != clang::BinaryOperatorKind::BO_Add) {
+      return true;
+    }
+    to_replace_.emplace_back(binary_operator, enclosing_function_);
+    return true;
+  }
+
+  // Return opportunities to replace + with -
+  const std::vector<std::pair<clang::BinaryOperator*, clang::FunctionDecl*>>&
+  GetReplacements() const {
+    return to_replace_;
   }
 
  private:
   const clang::ASTContext& ast_context_;
+
+  // Tracks the function currently being traversed; nullptr if there is no such
+  // function.
+  clang::FunctionDecl* enclosing_function_;
+
+  // As a proof of concept this currently stores opportunities for changing a +
+  // to a -.
+  std::vector<std::pair<clang::BinaryOperator*, clang::FunctionDecl*>>
+      to_replace_;
 };
 
 }  // namespace dredd
--- src/libdredd/src/mutate_ast_consumer.cc
+++ src/libdredd/src/mutate_ast_consumer.cc
@@ -14,14 +14,28 @@
 
 #include "libdredd/mutate_ast_consumer.h"
 
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <utility>
+#include <vector>
+
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
+#include "clang/AST/Expr.h"
+#include "clang/AST/PrettyPrinter.h"
+#include "clang/AST/Type.h"
 #include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Rewrite/Core/RewriteBuffer.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/ADT/StringRef.h"
 
 namespace dredd {
 
 void MutateAstConsumer::HandleTranslationUnit(clang::ASTContext& context) {
-  (void)compiler_instance_;
   (void)generator_;
   (void)num_mutations_;
   (void)output_file_;
@@ -30,6 +44,70 @@ void MutateAstConsumer::HandleTranslationUnit(clang::ASTContext& context) {
     return;
   }
   visitor_->TraverseDecl(context.getTranslationUnitDecl());
+  rewriter_.setSourceMgr(compiler_instance_.getSourceManager(),
+                         compiler_instance_.getLangOpts());
+
+  // Used to make each mutation wrapper function unique.
+  int count = 0;
+  // At present, all possible replacements are made. This should be changed so
+  // that a random subset of replacements are made, of the desired number of
+  // mutations. By construction, replacements are processed in a bottom-up
+  // fashion. This property should be preserved when the specific replacements
+  // are made at random, to avoid attempts to rewrite a child node after its
+  // parent has been rewritten.
+  for (auto replacement : visitor_->GetReplacements()) {
+    auto* binary_operator = replacement.first;
+
+    // The name of the mutation wrapper function to be used for this
+    // replacement. Right now add-to-sub is the only mutation supported.
+    std::string mutation_function_name("__dredd_add_to_sub_" +
+                                       std::to_string(count));
+
+    // Replace the binary operator expression with a call to the wrapper
+    // function.
+    rewriter_.ReplaceText(binary_operator->getSourceRange(),
+                          mutation_function_name + "(" +
+                              rewriter_.getRewrittenText(
+                                  binary_operator->getLHS()->getSourceRange()) +
+                              +", " +
+                              rewriter_.getRewrittenText(
+                                  binary_operator->getRHS()->getSourceRange()) +
+                              ")");
+
+    // Generate the wrapper function declaration and insert it before the
+    // enclosing function.
+    std::stringstream new_function;
+    const clang::BuiltinType* result_type =
+        binary_operator->getType()->getAs<clang::BuiltinType>();
+    assert(result_type != nullptr);
+    const clang::BuiltinType* lhs_type =
+        binary_operator->getLHS()->getType()->getAs<clang::BuiltinType>();
+    assert(lhs_type != nullptr);
+    const clang::BuiltinType* rhs_type =
+        binary_operator->getRHS()->getType()->getAs<clang::BuiltinType>();
+    assert(rhs_type != nullptr);
+    clang::PrintingPolicy printing_policy(compiler_instance_.getLangOpts());
+    new_function << "\n"
+                 << result_type->getName(printing_policy).str() << " "
+                 << mutation_function_name << "("
+                 << lhs_type->getName(printing_policy).str() << " arg1, "
+                 << rhs_type->getName(printing_policy).str() << " arg2"
+                 << ") {\n"
+                 << "  return arg1 - arg2;\n"
+                 << "}\n\n";
+
+    rewriter_.InsertTextBefore(replacement.second->getSourceRange().getBegin(),
+                               new_function.str());
+    count++;
+  }
+  // TODO(afd): This fails if no rewrites actually took place, so that should be
+  // accounted for.
+  const clang::RewriteBuffer* rewrite_buffer = rewriter_.getRewriteBufferFor(
+      compiler_instance_.getSourceManager().getMainFileID());
+  std::ofstream output_file_stream(output_file_, std::ofstream::out);
+  output_file_stream << std::string(rewrite_buffer->begin(),
+                                    rewrite_buffer->end());
+  output_file_stream.close();
 }
 
 }  // namespace dredd
