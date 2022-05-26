026b9b2b036958edcc098f95f9d63290caab5d68
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2022-05-26 17:10:02 +0100

    Introduce classes for mutations
    The proof-of-concept mutation now has its own class.

--- src/libdredd/src/mutate_ast_consumer.cc
+++ src/libdredd/src/mutate_ast_consumer.cc
@@ -14,24 +14,18 @@
 
 #include "libdredd/mutate_ast_consumer.h"
 
-#include <cassert>
 #include <fstream>
-#include <sstream>
-#include <utility>
 #include <vector>
 
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
-#include "clang/AST/Expr.h"
 #include "clang/AST/PrettyPrinter.h"
-#include "clang/AST/Type.h"
 #include "clang/Basic/Diagnostic.h"
-#include "clang/Basic/SourceLocation.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Rewrite/Core/RewriteBuffer.h"
 #include "clang/Rewrite/Core/Rewriter.h"
-#include "llvm/ADT/StringRef.h"
+#include "libdredd/mutation.h"
 
 namespace dredd {
 
@@ -47,6 +41,8 @@ void MutateAstConsumer::HandleTranslationUnit(clang::ASTContext& context) {
   rewriter_.setSourceMgr(compiler_instance_.getSourceManager(),
                          compiler_instance_.getLangOpts());
 
+  clang::PrintingPolicy printing_policy(compiler_instance_.getLangOpts());
+
   // Used to make each mutation wrapper function unique.
   int count = 0;
   // At present, all possible replacements are made. This should be changed so
@@ -55,49 +51,8 @@ void MutateAstConsumer::HandleTranslationUnit(clang::ASTContext& context) {
   // fashion. This property should be preserved when the specific replacements
   // are made at random, to avoid attempts to rewrite a child node after its
   // parent has been rewritten.
-  for (auto replacement : visitor_->GetReplacements()) {
-    auto* binary_operator = replacement.first;
-
-    // The name of the mutation wrapper function to be used for this
-    // replacement. Right now add-to-sub is the only mutation supported.
-    std::string mutation_function_name("__dredd_add_to_sub_" +
-                                       std::to_string(count));
-
-    // Replace the binary operator expression with a call to the wrapper
-    // function.
-    rewriter_.ReplaceText(binary_operator->getSourceRange(),
-                          mutation_function_name + "(" +
-                              rewriter_.getRewrittenText(
-                                  binary_operator->getLHS()->getSourceRange()) +
-                              +", " +
-                              rewriter_.getRewrittenText(
-                                  binary_operator->getRHS()->getSourceRange()) +
-                              ")");
-
-    // Generate the wrapper function declaration and insert it before the
-    // enclosing function.
-    std::stringstream new_function;
-    const clang::BuiltinType* result_type =
-        binary_operator->getType()->getAs<clang::BuiltinType>();
-    assert(result_type != nullptr);
-    const clang::BuiltinType* lhs_type =
-        binary_operator->getLHS()->getType()->getAs<clang::BuiltinType>();
-    assert(lhs_type != nullptr);
-    const clang::BuiltinType* rhs_type =
-        binary_operator->getRHS()->getType()->getAs<clang::BuiltinType>();
-    assert(rhs_type != nullptr);
-    clang::PrintingPolicy printing_policy(compiler_instance_.getLangOpts());
-    new_function << "\n"
-                 << result_type->getName(printing_policy).str() << " "
-                 << mutation_function_name << "("
-                 << lhs_type->getName(printing_policy).str() << " arg1, "
-                 << rhs_type->getName(printing_policy).str() << " arg2"
-                 << ") {\n"
-                 << "  return arg1 - arg2;\n"
-                 << "}\n\n";
-
-    rewriter_.InsertTextBefore(replacement.second->getSourceRange().getBegin(),
-                               new_function.str());
+  for (auto& replacement : visitor_->GetMutations()) {
+    replacement->Apply(count, rewriter_, printing_policy);
     count++;
   }
   // TODO(afd): This fails if no rewrites actually took place, so that should be
