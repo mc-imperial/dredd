cebf62322d92c7c41c5b1fcb8281e2cb6a6229b6
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2022-07-28 10:55:02 +0100

    Support comma-separated list of enabled mutants
    Allows multiple mutants to be enabled simultaneously via a
    comma-separated list.

---
+++ test/single_file/printing.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "A\n";
+  std::cout << "B\n";
+  std::cout << "C\n";
+  std::cout << "D\n";
+  std::cout << "E\n";
+  std::cout << "F\n";
+  std::cout << "G\n";
+}
