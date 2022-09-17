9e53326ca09949acd607592e1c715f127f43a06b
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-09-17 20:02:17 +0100

    Checkpoint.

--- scripts/check_one_single_file_test.sh
+++ scripts/check_one_single_file_test.sh
@@ -18,47 +18,55 @@ set -e
 set -u
 set -x
 
-if [ -z "${DREDD_SKIP_CHECK_COMPILE_COMMANDS+x}" ]
+DREDD_INSTALLED_EXECUTABLE=${DREDD_REPO_ROOT}/third_party/clang+llvm-13.0.1/bin/dredd
+
+# Move to the temporary directory
+cd "${DREDD_REPO_ROOT}"
+cd temp/
+
+if [ -z "${DREDD_SKIP_COPY_EXECUTABLE+x}" ]
 then
-  DREDD_INSTALLED_EXECUTABLE=${DREDD_REPO_ROOT}/third_party/clang+llvm-13.0.1/bin/dredd
-  # Move to the temporary directory
-  cd "${DREDD_REPO_ROOT}"
-  cd temp/
   # Ensure that Dredd is in its installed location. This depends on a
   # debug build being available
   cp build-Debug/src/dredd/dredd ${DREDD_INSTALLED_EXECUTABLE}
-  f="${DREDD_REPO_ROOT}/${1}"
-  # Copy the single-file test case to the temporary directory so
-  # that it can be mutated without affecting the original
-  cp $f .
-  copy_of_f=$(basename $f)      
-  # Mutate the test case using Dredd with optimisations
-  ${DREDD_INSTALLED_EXECUTABLE} --mutation-info-file temp.json ${copy_of_f} --
-  # Check that the mutated test case is as expected
-  diff ${copy_of_f} $f.expected
-  # Check that the mutated file compiles
-  if [[ $f == *.cc ]]
-  then
-    ${CXX} -c ${copy_of_f}
-  else
-    ${CC} -c ${copy_of_f}
-  fi
-  # Copy the single-file test case to the temporary directory so
-  # that it can be mutated without affecting the original
-  cp $f .
-  copy_of_f=$(basename $f)
-  # Mutate the test case using Dredd without optimisations
-  ${DREDD_INSTALLED_EXECUTABLE} --no-mutation-opts --mutation-info-file temp.json ${copy_of_f} --
-  # Check that the mutated test case is as expected
-  diff ${copy_of_f} $f.noopt.expected
-  # Check that the mutated file compiles
-  if [[ $f == *.cc ]]
-  then
-    ${CXX} -c ${copy_of_f}
-  else
-    ${CC} -c ${copy_of_f}
-  fi
-  # Clean up
-  rm ${copy_of_f}
-  rm ${copy_of_f%.*}.o
 fi
+
+f="${DREDD_REPO_ROOT}/${1}"
+
+# Copy the single-file test case to the temporary directory so
+# that it can be mutated without affecting the original
+cp $f .
+copy_of_f=$(basename $f)      
+# Mutate the test case using Dredd with optimisations
+${DREDD_INSTALLED_EXECUTABLE} --mutation-info-file temp.json ${copy_of_f} --
+# Check that the mutated test case is as expected
+diff ${copy_of_f} $f.expected
+# Check that the mutated file compiles
+if [[ $f == *.cc ]]
+then
+  ${CXX} -c ${copy_of_f}
+else
+  ${CC} -c ${copy_of_f}
+fi
+
+# Copy the single-file test case to the temporary directory so
+# that it can be mutated without affecting the original
+cp $f .
+copy_of_f=$(basename $f)
+
+# Mutate the test case using Dredd without optimisations
+${DREDD_INSTALLED_EXECUTABLE} --no-mutation-opts --mutation-info-file temp.json ${copy_of_f} --
+# Check that the mutated test case is as expected
+diff ${copy_of_f} $f.noopt.expected
+# Check that the mutated file compiles
+if [[ $f == *.cc ]]
+then
+  ${CXX} -c ${copy_of_f}
+else
+  ${CC} -c ${copy_of_f}
+fi
+
+# Clean up
+rm ${copy_of_f}
+rm ${copy_of_f%.*}.o
+
--- scripts/regenerate_single_file_expectations.sh
+++ scripts/regenerate_single_file_expectations.sh
@@ -21,47 +21,20 @@ set -x
 if [ -z "${DREDD_SKIP_CHECK_COMPILE_COMMANDS+x}" ]
 then
   DREDD_INSTALLED_EXECUTABLE=${DREDD_REPO_ROOT}/third_party/clang+llvm-13.0.1/bin/dredd
-  # Move to the temporary directory
-  cd "${DREDD_REPO_ROOT}"
-  cd temp/
+
+  cd ${DREDD_REPO_ROOT}
+  
   # Ensure that Dredd is in its installed location. This depends on a
   # debug build being available
-  cp build-Debug/src/dredd/dredd ${DREDD_INSTALLED_EXECUTABLE}
+  cp temp/build-Debug/src/dredd/dredd ${DREDD_INSTALLED_EXECUTABLE}
+
+  # Avoid copying Dredd to its installed location when invoking the script that
+  # checks a single test.
+  export DREDD_SKIP_COPY_EXECUTABLE=1
+  
   # Consider each single-file test case
-  for f in `ls ${DREDD_REPO_ROOT}/test/single_file/*.cc ${DREDD_REPO_ROOT}/test/single_file/*.c`
+  for f in `ls test/single_file/*.cc test/single_file/*.c`
   do
-    # Copy the single-file test case to the temporary directory so
-    # that it can be mutated without affecting the original
-    cp $f .
-    copy_of_f=$(basename $f)      
-    # Mutate the test case using Dredd
-    ${DREDD_INSTALLED_EXECUTABLE} --mutation-info-file temp.json ${copy_of_f} --
-    # Check that the mutated file compiles
-    if [[ $f == *.cc ]]
-    then
-      ${CXX} -c ${copy_of_f}
-    else
-      ${CC} -c ${copy_of_f}
-    fi
-    # Copy the mutated file so that it becomes the new test expectation
-    cp ${copy_of_f} $f.expected
-    # Copy the single-file test case to the temporary directory so
-    # that it can be mutated without affecting the original
-    cp $f .
-    copy_of_f=$(basename $f)
-    # Mutate the test case using Dredd
-    ${DREDD_INSTALLED_EXECUTABLE} --no-mutation-opts --mutation-info-file temp.json ${copy_of_f} --
-    # Check that the mutated file compiles
-    if [[ $f == *.cc ]]
-    then
-      ${CXX} -c ${copy_of_f}
-    else
-      ${CC} -c ${copy_of_f}
-    fi
-    # Copy the mutated file so that it becomes the new test expectation
-    cp ${copy_of_f} $f.noopt.expected
-    # Clean up
-    rm ${copy_of_f}
-    rm ${copy_of_f%.*}.o
+    regenerate_one_single_file_expectation.sh ${f}
   done
 fi
