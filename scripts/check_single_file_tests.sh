cc2da9bd3461c14d3af2be53d5c74c83aa64ea5d
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-09-23 09:32:20 +0100

    Progress.

--- scripts/check_single_file_tests.sh
+++ scripts/check_single_file_tests.sh
@@ -20,7 +20,7 @@ set -x
 
 if [ -z "${DREDD_SKIP_CHECK_COMPILE_COMMANDS+x}" ]
 then
-  DREDD_INSTALLED_EXECUTABLE=${DREDD_REPO_ROOT}/third_party/clang+llvm-14.0.6/bin/dredd
+  DREDD_INSTALLED_EXECUTABLE="${DREDD_REPO_ROOT}/third_party/clang+llvm-${DREDD_LLVM_TAG}/bin/dredd"
 
   cd "${DREDD_REPO_ROOT}"
   
