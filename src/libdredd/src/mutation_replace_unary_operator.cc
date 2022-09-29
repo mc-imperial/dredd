46b18d10d10288da12c5b49cd5baaa1f624cc485
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2022-09-29 10:17:44 +0100

    Remove unnecessary "unused" casts
    Avoids a couple of (void) casts for parameters that are now
    used. Tidies up punctuation in related comments.

--- src/libdredd/src/mutation_replace_unary_operator.cc
+++ src/libdredd/src/mutation_replace_unary_operator.cc
@@ -337,8 +337,6 @@ void MutationReplaceUnaryOperator::Apply(
     bool optimise_mutations, int first_mutation_id_in_file, int& mutation_id,
     clang::Rewriter& rewriter,
     std::unordered_set<std::string>& dredd_declarations) const {
-  (void)optimise_mutations;  // Unused
-
   std::string new_function_name =
       GetFunctionName(optimise_mutations, ast_context);
   std::string result_type = unary_operator_.getType()
