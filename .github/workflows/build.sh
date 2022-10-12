e45508ab88ef32fd400cc74c0deda0b9f1c29150
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-10-12 09:20:30 +0100

    Avoid running certain tests on Windows.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -141,21 +141,21 @@ case "$(uname)" in
   # due to differences in how certain builtin types, such as size_t and
   # uint64_t, expand. For simplicity, remove them before running single-file
   # tests on Windows.
-  #rm test/single_file/initializer_list.cc
-  #rm test/single_file/initializer_list.cc.expected
-  #rm test/single_file/initializer_list.cc.noopt.expected
-  #rm test/single_file/add_type_aliases.c
-  #rm test/single_file/add_type_aliases.c.expected
-  #rm test/single_file/add_type_aliases.c.noopt.expected
-  #rm test/single_file/add_type_aliases.cc
-  #rm test/single_file/add_type_aliases.cc.expected
-  #rm test/single_file/add_type_aliases.cc.noopt.expected
-  #rm test/single_file/positive_int_as_minus_one.c
-  #rm test/single_file/positive_int_as_minus_one.c.expected
-  #rm test/single_file/positive_int_as_minus_one.c.noopt.expected
-  #rm test/single_file/positive_int_as_minus_one.cc
-  #rm test/single_file/positive_int_as_minus_one.cc.expected
-  #rm test/single_file/positive_int_as_minus_one.cc.noopt.expected
+  rm test/single_file/initializer_list.cc
+  rm test/single_file/initializer_list.cc.expected
+  rm test/single_file/initializer_list.cc.noopt.expected
+  rm test/single_file/add_type_aliases.c
+  rm test/single_file/add_type_aliases.c.expected
+  rm test/single_file/add_type_aliases.c.noopt.expected
+  rm test/single_file/add_type_aliases.cc
+  rm test/single_file/add_type_aliases.cc.expected
+  rm test/single_file/add_type_aliases.cc.noopt.expected
+  rm test/single_file/positive_int_as_minus_one.c
+  rm test/single_file/positive_int_as_minus_one.c.expected
+  rm test/single_file/positive_int_as_minus_one.c.noopt.expected
+  rm test/single_file/positive_int_as_minus_one.cc
+  rm test/single_file/positive_int_as_minus_one.cc.expected
+  rm test/single_file/positive_int_as_minus_one.cc.noopt.expected
   DREDD_SKIP_COPY_EXECUTABLE=1 ./scripts/check_single_file_tests.sh
   ;;
 
