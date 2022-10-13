91dc6abcb702fe22a50c967a7abf9def6d0ca594
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2022-10-13 07:38:36 +0100

    Progress.

--- runner/main.py
+++ runner/main.py
@@ -6,10 +6,14 @@
 import argparse
 import functools
 import json
+import hashlib
+import os
 import random
+import subprocess
+import time
 
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Set
 
 class MutationTreeNode:
     def __init__(self, mutation_ids, children):
@@ -76,28 +80,144 @@ class MutationTree:
             result += self.nodes[node_id].mutation_ids
         return result
 
+class GeneratedProgramStats:
+    def __init__(self, compile_time: float, execution_time: float, expected_output: str,
+                 executable_hash: str):
+        self.compile_time = compile_time
+        self.execution_time = execution_time
+        self.expected_output = expected_output
+        self.executable_hash = executable_hash
+
+
+def hash_file(filename: str) -> str:
+    md5_hash = hashlib.md5()
+    md5_hash.update(open(filename, 'rb').read())
+    return md5_hash.hexdigest()
+
+
+def generate_a_program(csmith_root: Path,
+                       compiler_executable: Path) -> GeneratedProgramStats:
+    while True:
+        # TODO: cleanup
+        try:
+            # Run csmith until it yields a program
+            cmd: List[str] = [csmith_root / "build" / "src" / "csmith", "-o", "prog.c"]
+            result = subprocess.run(cmd, timeout=10, capture_output=True)
+            if result.returncode != 0:
+                print("csmith failed")
+                continue
+        except subprocess.TimeoutExpired:
+            print("csmith timed out")
+            continue
+
+        try:
+            cmd: List[str] = [compiler_executable, "-O3", "-I", csmith_root / "runtime", "-I", csmith_root / "build" / "runtime", "prog.c", "-o", "prog"]
+            compile_time_start: float = time.time()
+            result = subprocess.run(cmd, timeout=10, capture_output=True)
+            compile_time_end: float = time.time()
+            if result.returncode != 0:
+                print("compilation with non-mutated compiler failed")
+                continue
+        except subprocess.TimeoutExpired:
+            print("compilation with non-mutated compiler timed out")
+            continue
+
+        try:
+            cmd: List[str] = ["./prog"]
+            execute_time_start: float = time.time()
+            result = subprocess.run(cmd, timeout=10, capture_output=True)
+            execute_time_end: float = time.time()
+            if result.returncode != 0:
+                print("execution of generated program compiled with non-mutated compiler failed")
+                continue
+        except subprocess.TimeoutExpired:
+            print("execution of generated program compiled with non-mutated compiler timed out")
+            continue
+
+        return GeneratedProgramStats(compile_time=compile_time_end - compile_time_start,
+                                     execution_time=execute_time_end - execute_time_start,
+                                     expected_output=result.stdout.decode('utf-8'),
+                                     executable_hash=hash_file('prog'))
+
+
+def try_to_kill_mutants(csmith_root: Path,
+                        compiler_executable: Path,
+                        mutation_tree: MutationTree,
+                        unkilled_mutants: Set[int],
+                        untried_mutants: Set[int],
+                        program_stats: GeneratedProgramStats,
+                        num_simultaneous_mutations: int) -> bool:
+    available_mutants: Set[int] = set(untried_mutants)
+    selected_mutants: List[int] = []
+    while len(available_mutants) > 0 and len(selected_mutants) < num_simultaneous_mutations:
+        selected_mutant = list(available_mutants)[random.randrange(0, len(available_mutants))]
+        selected_mutants.append(selected_mutant)
+        available_mutants = available_mutants.difference(set(mutation_tree.get_incompatible_mutation_ids(selected_mutant)))
+
+    try:
+        cmd: List[str] = [compiler_executable, "-O3", "-I", csmith_root / "runtime", "-I",
+                          csmith_root / "build" / "runtime", "prog.c", "-o", "prog_mutated"]
+        mutated_environment: Dict[str, str] = os.environ.copy()
+        mutated_environment['DREDD_ENABLED_MUTATION'] = ",".join([str(m) for m in selected_mutants])
+        result = subprocess.run(cmd, timeout=5*program_stats.compile_time, capture_output=True,
+                                env=mutated_environment)
+        if result.returncode != 0:
+            print("KILL: Compilation with mutated compiler failed.")
+            for m in selected_mutants:
+                unkilled_mutants.remove(m)
+            return True
+
+        print("Compilation with mutated compiler succeeded")
+        if hash_file('prog_mutated') == program_stats.executable_hash:
+            print("Binaries are the same - not interesting")
+            return False
+        print("Different binaries!")
+        assert False
+
+    except subprocess.TimeoutExpired:
+        print("KILL: Compilation with mutated compiler timed out.")
+        for m in selected_mutants:
+            unkilled_mutants.remove(m)
+        return True
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("mutation_info_file", help="File containing information about mutations.", type=Path)
-    parser.add_argument("generation_command", help="Command for input generation.", type=str)
-    parser.add_argument("core_interestingness_test", help="Interestingness test to detect whether mutants are killed.", type=str)
+    parser.add_argument("compiler_executable", help="Path to the executable for the Dredd-mutated compiler.", type=Path)
+    parser.add_argument("csmith_root", help="Path to a checkout of Csmith, assuming that it has been built under 'build' beneath this directory.", type=Path)
+    parser.add_argument("--max_consecutive_failed_attempts_per_program", default=10, help="The number of times a given generated program will be compiled and executed in a row with no mutation being killed.", type=int)
+    parser.add_argument("--max_attempts_per_program", default=100, help="The number of times a given program will be compiled and executed to look for kills (excluding work done investigating kills) before moving on to the next program.", type=int)
+    parser.add_argument("--num_simultaneous_mutations", default=64, help="The number of mutations that will be enabled simultaneously when looking for kills.", type=int)
     args = parser.parse_args()
 
+    print("Building the mutation tree...")
     with open(args.mutation_info_file, 'r') as json_input:
-        tree = MutationTree(json.load(json_input))
+        mutation_tree = MutationTree(json.load(json_input))
+    print("Built!")
+    unkilled_mutants: Set[int] = set(range(0, mutation_tree.num_mutations))
 
-    available_mutations = list(range(0, tree.num_mutations))
-    selected_mutations = []
-    while len(selected_mutations) < 40000:
-        index = random.randrange(0, len(available_mutations))
-        mutation = available_mutations[index]
-        selected_mutations.append(mutation)
-        available_set = set(available_mutations)
-        incompatible_set = set(tree.get_incompatible_mutation_ids(mutation))
-        available_mutations = list(available_set.difference(incompatible_set))
-
-    print(",".join([str(m) for m in selected_mutations]))
+    while True:
+        print("Generating a program...")
+        program_stats: GeneratedProgramStats = generate_a_program(csmith_root = args.csmith_root,
+                           compiler_executable = args.compiler_executable)
+        print("Generated!")
+        untried_mutants: Set[int] = unkilled_mutants
+        num_attempts_for_current_program: int = 0
+        num_consecutive_failed_attempts_for_current_program: int = 0
+        while num_attempts_for_current_program < args.max_attempts_per_program and num_consecutive_failed_attempts_for_current_program < args.max_consecutive_failed_attempts_per_program:
+            success: bool = try_to_kill_mutants(csmith_root = args.csmith_root,
+                                                compiler_executable = args.compiler_executable,
+                                                mutation_tree = mutation_tree,
+                                                unkilled_mutants = unkilled_mutants,
+                                                untried_mutants = untried_mutants,
+                                                program_stats = program_stats,
+                                                num_simultaneous_mutations = args.num_simultaneous_mutations)
+            if not success:
+                num_consecutive_failed_attempts_for_current_program += 1
+            else:
+                num_consecutive_failed_attempts_for_current_program = 0
+            num_attempts_for_current_program += 1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
