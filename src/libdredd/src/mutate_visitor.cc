ee3d78ccb731bf5fda5df24a084359700b22cb15
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2023-11-13 10:34:44 +0000

    Fix.

--- src/libdredd/src/mutate_visitor.cc
+++ src/libdredd/src/mutate_visitor.cc
@@ -158,9 +158,19 @@ bool MutateVisitor::TraverseDecl(clang::Decl* decl) {
 }
 
 bool MutateVisitor::TraverseStmt(clang::Stmt* stmt) {
+  if (stmt == nullptr) {
+    return true;
+  }
+
   // Do not mutate under a constant expression, since mutation logic is
   // inherently non-constant.
-  if (stmt != nullptr && llvm::dyn_cast<clang::ConstantExpr>(stmt) != nullptr) {
+  if (llvm::dyn_cast<clang::ConstantExpr>(stmt) != nullptr) {
+    return true;
+  }
+
+  // Do not mutate under a 'noexcept', since it requires a constexpr argument
+  // and mutation logic is inherently non-constant.
+  if (llvm::dyn_cast<clang::CXXNoexceptExpr>(stmt) != nullptr) {
     return true;
   }
 
@@ -369,6 +379,10 @@ bool MutateVisitor::HandleBinaryOperator(
 }
 
 bool MutateVisitor::VisitExpr(clang::Expr* expr) {
+  assert(llvm::dyn_cast<clang::CXXNoexceptExpr>(expr) == nullptr &&
+         "A 'noexcept' expression should be skipped when deciding where to "
+         "mutate.");
+
   if (optimise_mutations_) {
     // There is no value in mutating a parentheses expression.
     if (llvm::dyn_cast<clang::ParenExpr>(expr) != nullptr) {
