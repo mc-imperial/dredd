a24aa1d8912e1df806f833fa9ad78194026a874c
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2023-11-20 11:00:22 +0000

    Fix to remove statement mutation
    Works around an issue where the semicolon for a statement is not
    captured by the conditional that wraps a statement if the statement is
    separated from its semicolon by a preprocessor directive.
    
    Fixes #189.

--- src/libdredd/include/libdredd/mutation_remove_stmt.h
+++ src/libdredd/include/libdredd/mutation_remove_stmt.h
@@ -20,6 +20,7 @@
 
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Stmt.h"
+#include "clang/Basic/SourceLocation.h"
 #include "clang/Lex/Preprocessor.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "libdredd/mutation.h"
@@ -42,6 +43,14 @@ class MutationRemoveStmt : public Mutation {
       std::unordered_set<std::string>& dredd_declarations) const override;
 
  private:
+  // Helper method to determine whether the token immediately following the
+  // given source range is the '#' token. This is useful for working around
+  // issues where the placement of semicolons at the end of a statement that is
+  // wrapped in a conditional is challenging, due to a preprocessor directive
+  // occurring between the end of a statement and its associated semicolon.
+  static bool IsNextTokenHash(const clang::CharSourceRange& source_range,
+                              const clang::Preprocessor& preprocessor);
+
   const clang::Stmt* stmt_;
   InfoForSourceRange info_for_source_range_;
 };
---
+++ test/single_file/preprocessor_if.c
@@ -0,0 +1,11 @@
+int main() {
+
+    int ABC;
+    
+    ABC = 2
+    #if 1
+        | 5
+    #endif
+        ;
+
+}
