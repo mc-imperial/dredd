9a858eeefbbfccc9164429279e329881a840df67
Ubuntu <ubuntu@ip-172-31-46-117.eu-west-2.compute.internal>
2024-05-24 11:31:16 +0000

    Feat: change file and directory name

--- test/compile_execute_file/bit_overflow.runner.sh
+++ test/compile_execute_file/bit_overflow.runner.sh
@@ -12,10 +12,10 @@ ${DREDD_EXECUTABLE} ${SCRIPTPATH}/bit_overflow.c --mutation-info-file ${SCRIPTPA
 # compile mutated binary
 ${DREDD_CLANG_BIN_DIR}/clang ${SCRIPTPATH}/bit_overflow.c -o ${SCRIPTPATH}/bit_overflow.o
 
-DREDD_ENABLED_MUTATION=32 ${SCRIPTPATH}/bit_overflow.o > ${SCRIPTPATH}/bit_overflow.out.actual
+DREDD_ENABLED_MUTATION=32 ${SCRIPTPATH}/bit_overflow.o > ${SCRIPTPATH}/bit_overflow.c.actual
 
 # Compare actual output with expected output
-if diff ${SCRIPTPATH}/bit_overflow.out.expected ${SCRIPTPATH}/bit_overflow.out.actual; then
+if diff ${SCRIPTPATH}/bit_overflow.c.expected ${SCRIPTPATH}/bit_overflow.c.actual; then
     echo "Test Passed"
 else
     echo "Test Failed"
@@ -23,7 +23,7 @@ fi
 
 # cleanup
 rm ${SCRIPTPATH}/temp.json
-rm ${SCRIPTPATH}/bit_overflow.out.actual
+rm ${SCRIPTPATH}/bit_overflow.c.actual
 rm ${SCRIPTPATH}/bit_overflow.o
 mv ${SCRIPTPATH}/bit_overflow.orig.c ${SCRIPTPATH}/bit_overflow.c 
 
