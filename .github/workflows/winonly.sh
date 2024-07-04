1f07e565a2e137089c65a98c57d17ca46dabdd29
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2024-07-04 10:18:41 +0100

    Windows workflows.

---
+++ .github/workflows/winonly.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 The Dredd Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+set -e
+set -u
+
+help | head
+
+uname
+
+cl.exe /std:c++20 tests/single_file/printing.cc
+cp tests/single_file/printing.cc.expected temp.cc
+cl.exe /std:c++20 temp.cc
