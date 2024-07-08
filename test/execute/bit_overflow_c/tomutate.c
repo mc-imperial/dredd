69d7a4eedad74064bef81708aa7a38fb71652fd0
Ubuntu <ubuntu@cloud-vm-43-147.doc.ic.ac.uk>
2024-07-08 19:35:27 +0100

    Feat: integrate bit_overflow testcase into new check_execute_tests CI test

---
+++ test/execute/bit_overflow_c/tomutate.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void print_33_nums() {
+    printf("0_");
+    printf("1_");
+    printf("2_");
+    printf("3_");
+    printf("4_");
+    printf("5_");
+    printf("6_");
+    printf("7_");
+    printf("8_");
+    printf("9_");
+    printf("10_");
+    printf("11_");
+    printf("12_");
+    printf("13_");
+    printf("14_");
+    printf("15_");
+    printf("16_");
+    printf("17_");
+    printf("18_");
+    printf("19_");
+    printf("20_");
+    printf("21_");
+    printf("22_");
+    printf("23_");
+    printf("24_");
+    printf("25_");
+    printf("26_");
+    printf("27_");
+    printf("28_");
+    printf("29_");
+    printf("30_");
+    printf("31_");
+    printf("32_");
+}
