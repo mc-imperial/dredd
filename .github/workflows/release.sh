8fc3827e95f4cf7e7bc75a9f4493883989502f24
Alastair Donaldson <alastair.donaldson@imperial.ac.uk>
2025-06-20 10:18:19 +0100

    Update release workflow

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -25,59 +25,22 @@ help | head
 
 uname
 
-export DREDD_LLVM_SUFFIX=""
-DREDD_LLVM_TAG=$(./scripts/llvm_tag.sh)
-
 case "$(uname)" in
 "Linux")
   NINJA_OS="linux"
-  export DREDD_LLVM_SUFFIX="-stock-clang"
   PYTHON="python3"
   DREDD_RELEASE_OS="${OS}"
+
+  sudo apt update
+  sudo apt install -y llvm-17 clang-17 clang-tidy-17 clang-format-17 libclang-17-dev
+  
   export CC=clang
   export CXX=clang++
   # Free up some space
   df -h
   sudo apt clean
-  # shellcheck disable=SC2046
-  docker rmi -f $(docker image ls -aq)
   sudo rm -rf /usr/share/dotnet /usr/local/lib/android /opt/ghc
   df -h
-
-  # Install clang.
-  DREDD_LLVM_TAG=$(./scripts/llvm_tag.sh)
-  pushd ./third_party/clang+llvm
-    curl -fsSL -o clang+llvm.tar.xz "https://github.com/llvm/llvm-project/releases/download/llvmorg-${DREDD_LLVM_TAG}/clang+llvm-${DREDD_LLVM_TAG}-x86_64-linux-gnu-ubuntu-22.04.tar.xz"
-    tar xf clang+llvm.tar.xz
-    mv clang+llvm-${DREDD_LLVM_TAG}-x86_64-linux-gnu-ubuntu-22.04/* .
-    rm clang+llvm.tar.xz
-  popd
-  ;;
-
-"Darwin")
-  # Install clang.
-  pushd ./third_party/clang+llvm
-    curl -fsSL -o clang+llvm.zip "https://github.com/mc-imperial/build-clang/releases/download/llvmorg-${DREDD_LLVM_TAG}/build-clang-llvmorg-${DREDD_LLVM_TAG}-Mac_x64_Release.zip"
-    unzip clang+llvm.zip
-    rm clang+llvm.zip
-  popd
-  NINJA_OS="mac"
-  PYTHON="python3"
-  DREDD_RELEASE_OS="Mac"
-  ;;
-
-"MINGW"*|"MSYS_NT"*)
-  NINJA_OS="win"
-  PYTHON="python"
-  DREDD_RELEASE_OS="Windows"
-  CMAKE_OPTIONS+=("-DCMAKE_C_COMPILER=cl.exe" "-DCMAKE_CXX_COMPILER=cl.exe")
-  choco install zip
-  # Install clang.
-  pushd ./third_party/clang+llvm
-    curl -fsSL -o clang+llvm.zip "https://github.com/mc-imperial/build-clang/releases/download/llvmorg-${DREDD_LLVM_TAG}/build-clang-llvmorg-${DREDD_LLVM_TAG}-Windows_x64_Release.zip"
-    unzip clang+llvm.zip
-    rm clang+llvm.zip
-  popd
   ;;
 
 *)
@@ -85,8 +48,6 @@ case "$(uname)" in
   exit 1
   ;;
 esac
-
-export PATH="$(pwd)/third_party/clang+llvm/bin:${HOME}/bin:$PATH"
 
 mkdir -p "${HOME}/bin"
 
@@ -108,17 +69,12 @@ CMAKE_BUILD_TYPE="${CONFIG}"
 
 BUILD_DIR="b_${CONFIG}"
 
-mkdir -p "${BUILD_DIR}"
-pushd "${BUILD_DIR}"
-cmake .. -G "${CMAKE_GENERATOR}" "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" "${CMAKE_OPTIONS[@]}"
-cmake --build . --config "${CMAKE_BUILD_TYPE}"
-popd
+cmake -S . -B ${BUILD_DIR} -G Ninja -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DDREDD_CLANG_LLVM_DIR=/usr/lib/llvm-17
+cmake --build ${BUILD_DIR} --config ${CMAKE_BUILD_TYPE}
 
-# Get the files actually needed for the release: the dredd executable, and the header files that ship with Clang
+# Get the file actually needed for the release: the dredd executable
 mkdir -p dredd/bin
 cp "${BUILD_DIR}/src/dredd/dredd" dredd/bin/dredd
-mkdir -p "dredd/lib/clang/${DREDD_LLVM_TAG}"
-cp -r "third_party/clang+llvm/lib/clang/${DREDD_LLVM_TAG}/include" "dredd/lib/clang/${DREDD_LLVM_TAG}"
 DREDD_ZIP_NAME="dredd-${DREDD_RELEASE_OS}-${CONFIG}.zip"
 zip -r "${DREDD_ZIP_NAME}" dredd
 
@@ -132,7 +88,7 @@ DESCRIPTION="$(echo -e "Automated build for dredd revision ${GITHUB_SHA}.")"
 "${PYTHON}" -m github_release_retry.github_release_retry \
   --user "mc-imperial" \
   --repo "dredd" \
-  --tag_name "0.2" \
+  --tag_name "1.0" \
   --target_commitish "${GITHUB_SHA}" \
   --body_string "${DESCRIPTION}" \
   "${DREDD_ZIP_NAME}"
